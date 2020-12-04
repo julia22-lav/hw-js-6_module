7c84661ef8954b7110bd38b6bc5f2d97282f62cd
Julia Lavrenko <lavrenkojulia@gmail.com>
2020-12-04 11:26:15 +0100

    Task 2 is added

--- 6_module-1.js
+++ 6_module-1.js
@@ -1,12 +1,12 @@
-const users = [
- { name: "Moore Hensley" },
-  { name: "Sharlene Bush"} ,
-  { name: "Ross Vazquez" },
-  { name: "Elma Head"} ,
-  {name: "Carey Barr" },
-  { name: "Blackburn Dotson" },
-  { name: "Sheree Anthony"}
- ];
-const {name} = users;
-const getUserNames = (array) => array.map(({ name }) => name);
-console.log(getUserNames(users));
+// const users = [
+//  { name: "Moore Hensley" },
+//   { name: "Sharlene Bush"} ,
+//   { name: "Ross Vazquez" },
+//   { name: "Elma Head"} ,
+//   {name: "Carey Barr" },
+//   { name: "Blackburn Dotson" },
+//   { name: "Sheree Anthony"}
+//  ];
+// const {name} = users;
+// const getUserNames = (array) => array.map(({ name }) => name);
+// console.log(getUserNames(users));
